b96e0636bc7c293c9040ff381bf0441ba6e58b47
John Hightower <john.hightower@uafs.edu>
2024-03-23 16:46:58 -0500

    Updated

--- views/userList.php
+++ views/userList.php
@@ -4,7 +4,7 @@
             <a href="start.php?action=userAdd" class="btn btn-primary">Add User</a>
             <button class="btn btn-primary" type="submit" name="action" value="userUpdate">Update User</button>
             <button class="btn btn-primary" type="submit" name="action" value="userDelete">Delete User</button>
-            <table class="table table-bordered table-striped">
+            <table class="table table-bordered table-striped mt-3">
                 <thead><tr><th class="text-center">User ID</th><th>Last Name</th><th>First Name</th><th>User Name</th><th>Password</th>
                 <th>Email</th><th>Role</th></tr></thead>
                 <tbody>
