2bb06979128480c81232598253df1f716cea2dbf
Landon Brady <cbrady01@uafs.edu>
2024-03-27 10:44:08 -0500

    update

--- start.php
+++ start.php
@@ -8,6 +8,7 @@
     include_once "./controllers/UserUpdate.php";
     include_once "./controllers/UserDelete.php";
     include_once "./controllers/Feature.php";
+    include_once "./controllers/Login.php";
 
     $router = new Router();
     $router->showErrors(0);
@@ -15,6 +16,7 @@
     $router->addController('home',new Home());
     $router->addController('about',new About());
     $router->addController('feature',new Feature());
+    $router->addController('login',new Login());
     $router->addController('userList',new UserList());
     $router->addController('userAdd',new UserAdd());
     $router->addController('userUpdate',new UserUpdate());
