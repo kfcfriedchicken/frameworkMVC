f31845c48f7da9bf7c9aa3650df012c5318b96de
Landon Brady <cbrady01@uafs.edu>
2024-03-29 10:25:38 -0500

    update

--- controllers/Login.php
+++ controllers/Login.php
@@ -3,5 +3,10 @@ class Login extends Controller{
     public function performAction(){
         $this->renderView('login',[]);
     }
+
+    public function renderView($view,$data=[]){
+        
+        include "./views/$view.php";
+    }
 }
 ?>
