9b3ee470a09f3357e758b5a8f915926baf9ef83d
Landon Brady <cbrady01@uafs.edu>
2024-04-10 10:52:34 -0500

    update

--- controllers/Login.php
+++ controllers/Login.php
@@ -1,30 +1,35 @@
 <?php
+include_once "./models/UserDAO.php";
+
 class Login extends Controller{
+    private $userDAO;
+    private $result;
+
     public function performAction(){
-        if($_SERVER['REQUEST_METHOD']=="GET"){
-            $this->renderView('login',[]);
+        if($_SERVER['REQUEST_METHOD']=='GET'){
+            $this->renderView("login",[]);
         }else{
             if(isset($_POST['username']) && isset($_POST['passwd'])){
                 $username = $_POST['username'];
                 $passwd = $_POST['passwd'];
-
                 $userDAO = new UserDAO();
-                $result = $userDAO->authenticate($username,$passwd);
+                $result=$userDAO->authenticate($username,$passwd);
                 if($result==NULL){
-                    header("Location: start.php?action=login");
+                    //** Unsuccessful */
+                    header('Location: start.php?action=login');
                     exit;
                 }else{
+                    //** Successful */
                     $_SESSION['loggedin']=$result;
-                    header("Location: start.php?action=home");
+                    header('Location: start.php?action=home');
                     exit;
                 }
             }
+            
         }
-
     }
-
+    //*** Override renderView from Controller.php */
     public function renderView($view,$data=[]){
-        
         include "./views/$view.php";
     }
 }
